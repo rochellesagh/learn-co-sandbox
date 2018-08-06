7e0b5d7959c15e5d4588c8c251b53fe57a246ba2
rochellesagh <rsrochelli@gmail.com>
2018-08-06 20:16:28 +0000

    Done.

---
+++ methods.rb
@@ -0,0 +1,16 @@
+# def about_me
+# puts "Hi! My name is Rochelle."
+# puts "I am 17 years old"  
+# puts "I live in New York"
+# puts "My favorite TV show is Game of Thrones"
+# end 
+
+# about_me
+# about_me 
+
+def greeting
+  local_greeting ="Hello"
+  puts "#{local_greeting} I hope you have a great day!"
+end 
+
+greeting 
