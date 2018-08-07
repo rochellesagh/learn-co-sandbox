123055f7fde1762574a88163a92472822b496096
rochellesagh <rsrochelli@gmail.com>
2018-08-07 17:28:48 +0000

    Done.

--- conditionals.rb
+++ conditionals.rb
@@ -1,1 +1,16 @@
-puts "Does this still work?"
+#curfew checker
+#if the time is before 7pm (19), say "You're good, stay out!"
+practice_time = 23
+
+if practice_time <19
+  puts "You're good, stay out!"
+end 
+
+# if the time is after 7pm (19), say "You're late :("
+if practice_time > 19 && practice_time < 22
+  puts "You're late :("
+end 
+
+if practice_time > 22
+  puts "You're incredibly late :( :( :( "
+end 
---
+++ shoes.rb
@@ -0,0 +1,17 @@
+#shoe size checker 
+# if shoe size is less than 7 => :(
+# if shoe size is between 7 && 9.5 ==> "Yay! New Shoes"
+# if shoe size is over 9.5 => "booooo"
+shoe_size = 8
+
+if shoe_size <7 
+  puts ":("
+  end 
+  
+  if shoe_size >= 7 && shoe_size <= 9.5 
+    puts "Yay! New Shoes"
+  end 
+  
+  if shoe_size > 9.5
+    puts "booooo"
+  end
