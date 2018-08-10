35a80590e221a1b021e9dd6409515599ff9c8695
rochellesagh <rsrochelli@gmail.com>
2018-08-10 14:11:59 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -8,3 +8,24 @@ counter = counter + 1
 end 
 
 
+computer_array = [1, 2, 4, 6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
+
+#STEP 1
+# Simulate the rolling of a die 5 times
+counter = 0 
+while counter <30 
+
+# STEP 2
+#Push each roll to an array
+
+# STEP 3
+#Print out the array
+
+# STEP 4
+# Print out the total of your 5 rolls
+
+# STEP 5
+# Compare your total to the computer’s total (a random number chosen from the computer_array)
+
+# STEP 6
+# Determine the winner/if it is a tie and output a message that congratulates the winner!
