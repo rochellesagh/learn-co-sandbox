862c125693bfdd71f0ecac585d395a4243bfc07c
rochellesagh <rsrochelli@gmail.com>
2018-08-10 15:04:29 +0000

    Done.

--- dice.rb
+++ dice.rb
@@ -1,10 +1,55 @@
-
+#STEP 1
+# Simulate the rolling of a die 5 times
 dice = [1, 2, 3, 4, 5, 6]
+
+# STEP 2
+#Push each roll to an array
+rochelles_rolls = []
 
 counter = 0 
 while counter <5 
-puts dice.sample
+  # puts dice.sample
+  rochelles_rolls.push(dice.sample)
 counter = counter + 1 
 end 
 
 
+# STEP 3
+#Print out the array
+puts rochelles_rolls
+
+
+
+# STEP 4
+# Print out the total of your 5 rolls
+total= 0 
+rochelles_rolls.each do |roll|
+  # .each isolates each roll 
+  total = total + roll 
+  #only use string interpolation with strings, USE THIS FOR INTEGERS 
+  
+  
+end 
+
+puts total
+
+# computer_array = [1, 2, 4, 6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
+
+
+# STEP 5
+# Compare your total to the computer’s total (a random number chosen from the computer_array)
+
+computer_array = [6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
+computer_total = computer_array.sample 
+puts computer_total
+
+# STEP 6
+# Determine the winner/if it is a tie and output a message that congratulates the winner!
+ 
+if total > computer_total 
+puts "Congratulations! You Won!"
+elsif computer_total< total 
+puts "Sorry! You Lost.:("
+elsif total == computer_total 
+puts "It's a tie!"
+end 
