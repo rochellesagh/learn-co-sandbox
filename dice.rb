a5636fadc31cb2e71792e8710f718fbe415b86f0
rochellesagh <rsrochelli@gmail.com>
2018-08-10 14:37:01 +0000

    automatically backed up by learn

--- dice.rb
+++ dice.rb
@@ -1,7 +1,6 @@
 
 dice = [1, 2, 3, 4, 5, 6]
 rochelles_rolls = []
-rochelles_total =[]
 
 counter = 0 
 while counter <5 
@@ -12,18 +11,21 @@ end
 
 puts rochelles_rolls
 
-counter2 = 0
-while counter2 <5 
- rochelles_total = 0
-  eval([rochelles_rolls].join('+'))
+total= 0 
+rochelles_rolls.each do |roll|
+  # .each isolates each roll 
+  total = total + roll 
+  
 end 
+
+
 
 computer_array = [1, 2, 4, 6, 13, 17, 18, 20, 22, 30, 29, 6, 8, 13, 14, 15, 17, 14, 16, 12, 22]
 
 #STEP 1
 # Simulate the rolling of a die 5 times
-counter = 0 
-while counter <30
+# counter = 0 
+# while counter <30
 
 # STEP 2
 #Push each roll to an array
